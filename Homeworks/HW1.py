1227d8c5a0bcaa52bf9706c18ef58774b9057bcb
furkanbogaci <49916138+furkanbogaci@users.noreply.github.com>
2021-02-17 10:44:10 +0300

    Add files via upload

---
+++ Homeworks/HW1.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy import random
+matrix = [[],[],[]]
+
+for i in range(0,3):
+  for j in range(0,3):
+      for k in np.random.randint(0,10,1):
+        matrix[i].append(k)
+
+for i in range(3):
+      print(matrix[i])
